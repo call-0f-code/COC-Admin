ac04ce84e065705776b8df1f0d6cbe4012adcc4b
shrutiii <shrutijadhav4102@gmail.com>
2025-07-31 23:58:33 +0530

    Reviewed OLD comments

--- backend/src/controllers/member.controller.ts
+++ backend/src/controllers/member.controller.ts
@@ -5,40 +5,30 @@ import bcrypt from 'bcrypt';
 import axios from "axios";
 import config from "../config";
 import jwt from 'jsonwebtoken';
+import { ApiError } from "../utils/apiError";
 
 
 export const login = async(req: Request, res:Response) => {
 
     const parsedData = SigninSchema.safeParse(req.body);
     if (!parsedData.success) {
-      return res.status(400).json({
-        success: false,
-        message: parsedData.error.message,
-      });
+      throw new ApiError(parsedData.error.message, 500);
     }
 
     const { email, password } = parsedData.data;
     const check = await axios.get(`${config.API_URL}/api/v1/members/?email=${email}&password=${password}`);
 
-    if(!check.data.success) {
-      return res.status(400).json({message: "Error signing in"});
-    }
     const adminId = check.data.user.id;
     const hashedPassword = check.data.user.accounts[0];
     const isManager = check.data.user.isManager;
 
     if(!isManager) {
-      return res.status(403).json({
-        success: false,
-        message: "Unauthorized access detected"
-      })
+      throw new ApiError("Unauthorized access detected", 403);
     }
+    
     const isPasswordValid = await bcrypt.compare(password, hashedPassword.password);
     if(!isPasswordValid) {
-      return res.status(403).json({
-        success: false,
-        message: "Invalid password"
-      })
+        throw new ApiError("Invalid password", 403)
     }
 
     // Generate JWT token
@@ -57,10 +47,7 @@ export const getunapprovedMembers = async(req: Request, res: Response) => {
   const members = await api.get('members/unapproved');
 
   if(!members.data.success) {
-    return res.status(400).json({
-        success: false,
-        message: members.data.message
-    })
+        throw new ApiError(members.data.message, 500);
   }
 
   res.json({
@@ -78,11 +65,7 @@ export const approveMember = async(req: Request, res: Response) => {
 
     const approval = await api.patch(`/members/approve/${memberId}`, {isApproved, adminId});
     if(!approval.data.success) {
-        res.status(400).json({
-            success: false,
-            message: approval.data.message
-        })
-        return;
+      throw new ApiError(approval.data.message, 500);
     }
     
     res.status(200).json({
