7a5f425891e10f6e32881b3f75d9f6c1092e4683
mrforgotten <harishnarote7@gmail.com>
2025-08-01 13:11:25 +0530

    Enhance environment variable handling by adding checks for JWT_SECRET, SALTING, and API_URL

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,7 +1,22 @@
 export default{
     port: process.env.PORT || 3000,
     allowed_origins: process.env.ALLOWED_ORIGINS || "*",
-    JWT_SECRET: process.env.JWT_SECRET!,
-    SALTING: process.env.SALTING!,
-    API_URL: process.env.API_URL!
+    JWT_SECRET: ()=>{
+        if (!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET is not defined in environment variables");
+        }
+        return process.env.JWT_SECRET;
+    },
+    SALTING: ()=>{
+        if (!process.env.SALTING) {
+            throw new Error("SALTING is not defined in environment variables");
+        }
+        return process.env.SALTING;
+    },
+    API_URL:()=>{
+        if (!process.env.API_URL) {
+            throw new Error("API_URL is not defined in environment variables");
+        }
+        return process.env.API_URL;
+    }
 }
