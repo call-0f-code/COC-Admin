05a7857e3d2d6ccc6bec2b1a1c7c57da1bc2ffe6
mrforgotten <harishnarote7@gmail.com>
2025-09-13 22:29:42 +0530

    update types for questions and topics

--- frontend/src/Components/dsa/topics/TopicForm.tsx
+++ frontend/src/Components/dsa/topics/TopicForm.tsx
@@ -9,6 +9,7 @@ interface topicFormProps {
   onCancel: () => void;
   onSuccess: () => void;
   isEditing: boolean;
+  editingTopicId :string | null;
 }
 
 export const TopicForm :React.FC<topicFormProps> = ({ 
@@ -16,16 +17,22 @@ export const TopicForm :React.FC<topicFormProps> = ({
   setTopicForm,  
   onCancel, 
   onSuccess,
-  isEditing = false 
+  isEditing = false,
+  editingTopicId
 }) => {
   const {createNewTopic,updateCurrentTopic} = useTopics();
   const onSave = () => {
     if (!topicForm?.title?.trim()) return;
     
+    const topic : any = topicForm;
     const mutation = isEditing ? updateCurrentTopic : createNewTopic;
+    if(isEditing){
+      topic.id = editingTopicId;
+    }
     const msg = isEditing ? "Topic Updated Successfully" : "Topic Created Succeddfully";
+   
     
-    mutation.mutate(topicForm, {
+    mutation.mutate(topic, {
       onSuccess: () => {
         globalToast.success(msg)
         onSuccess(); 
